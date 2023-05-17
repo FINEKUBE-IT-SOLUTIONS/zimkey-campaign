a8515420756f08c49e1637eb831bed2e547faa16
Umar Sanil <71665435+UmerEdava@users.noreply.github.com>
2023-05-17 18:54:16 +0530

    gtag manager added in head of index page

--- pages/_app.js
+++ pages/_app.js
@@ -30,6 +30,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"></link>
         <link rel="stylesheet" href="/public/assets/css/custom.css" />
+
       </Head>
       <Script
         src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
